9c12fa05cca011e7b0c3fb74a853a3f607753bc3
Bruna Drago <bruna_drago@hotmail.com>
2021-11-02 17:32:50 -0300

    add doc to protocol

--- solutions/devsprint-1/DeliveryAppChallenge/Shared/ViewCode.swift
+++ solutions/devsprint-1/DeliveryAppChallenge/Shared/ViewCode.swift
@@ -7,14 +7,18 @@
 
 import Foundation
 
+/// Este protocolo implementa as funções necessárias para a construção de uma View.
 protocol ViewCode {
     
+    /// Esta função deverá ser chamada no método viewDidLoad para implementar o código da extension
     func setup()
     
+    /// Esta função deverá ser usada para adicionar a hierárquia de subviews
     func setupComponents()
     
     func setupConstraints()
     
+    /// Esta função deverá ser usada para implementar configurações adicionais , como acessibilidade, cor, targets
     func setupExtraConfiguration()
 }
 
