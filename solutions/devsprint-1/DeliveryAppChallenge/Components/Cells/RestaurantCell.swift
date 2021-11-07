1747051dad5560afc004b88e1417f81cc03b4fcd
Edson Neto <edson_neto1996@hotmail.com>
2021-11-07 16:31:41 -0300

    Making the initial configuration

---
+++ solutions/devsprint-1/DeliveryAppChallenge/Components/Cells/RestaurantCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantCell.swift
+//  DeliveryAppChallenge
+//
+//  Created by Edson Neto on 04/11/21.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+    
+    
+}
+
+extension RestaurantCell: ViewCode{
+    
+    func setupComponents() {
+        //image view restaurant logo
+        // uitext restaurant name
+        // cell has subtitles and chevron to go to the restaurant detail
+    }
+    
+    func setupConstraints() {
+        <#code#>
+    }
+    
+}
