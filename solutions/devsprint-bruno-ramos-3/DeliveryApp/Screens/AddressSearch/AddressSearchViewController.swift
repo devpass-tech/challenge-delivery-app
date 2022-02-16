13860d86935da2d8a93be159269ddeea366d54da
Daniel Araujo <danielaraujos@live.com>
2022-02-16 13:49:48 -0300

    Change  initializer and properties of screen

--- solutions/devsprint-bruno-ramos-3/DeliveryApp/Screens/AddressSearch/AddressSearchViewController.swift
+++ solutions/devsprint-bruno-ramos-3/DeliveryApp/Screens/AddressSearch/AddressSearchViewController.swift
@@ -9,16 +9,33 @@ import UIKit
 
 class AddressSearchViewController: UIViewController {
 
-    init() {
+    private let customView: AddressListViewProtocol
+    
+    
+    init(customView: AddressListViewProtocol) {
+        self.customView = customView
         super.init(nibName: nil, bundle: nil)
-
+    }
+    
+    override func loadView() {
+        self.view = customView
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.title = "Search"
+    }
+}
 
-    override func loadView() {
-        self.view = AddressListView()
+
+extension AddressSearchViewController: AddressListViewDelegate {
+    func didTapOnSomeButton() {
+        //code
     }
 }
