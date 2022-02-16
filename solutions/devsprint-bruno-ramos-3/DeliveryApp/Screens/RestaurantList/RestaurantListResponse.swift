ca9bd72be6408037158149b7eeae8cb00d633ff0
lysonjeada <contatotechlys@gmail.com>
2022-02-16 16:04:39 -0300

    Adding RestaurantResponse

--- solutions/devsprint-bruno-ramos-3/DeliveryApp/Screens/RestaurantList/RestaurantListResponse.swift
+++ solutions/devsprint-bruno-ramos-3/DeliveryApp/Screens/RestaurantList/RestaurantListResponse.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct RestaurantResponse: Decodable {
+    
+    let name: String
+    let category: String
+    let deliveryTime: DeliveryTime
+    
+    private enum CodingKeys: String, CodingKey {
+        case name, category
+        case deliveryTime = "delivery_time"
+    }
+}
