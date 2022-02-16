34987b0be3ef3cd8ebda6a97abab9377eff02373
sarafelix <x283609@prservicos.com.br>
2022-02-16 12:21:02 -0300

    incluindo comentários

--- solutions/devsprint-bruno-ramos-3/DeliveryApp/Screens/Components/FeatureFactory.swift
+++ solutions/devsprint-bruno-ramos-3/DeliveryApp/Screens/Components/FeatureFactory.swift
@@ -10,5 +10,11 @@ import UIKit
 protocol FeatureFactory {
     associatedtype Dependencies
     
+    /// Esta função retorna um view controller construído para determinadas dependências
+    
+    
+    /// - Aviso: nenhum.
+    /// - Dependências de parâmetro: Dependências injetadas pelo recurso.
+    /// - Returns: Um view controller construído para ser apresentado usando `dependencies`.
     static func make(with dependencies: Dependencies) -> UIViewController
 }
