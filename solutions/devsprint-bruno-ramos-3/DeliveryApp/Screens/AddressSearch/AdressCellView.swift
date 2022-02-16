cb3180f59fee06b43f19011aebd1a3074a1fb342
Pedro Leda <pedro.henrique.leda@gmail.com>
2022-02-15 22:10:24 -0300

    adjusted constraints

--- solutions/devsprint-bruno-ramos-3/DeliveryApp/Screens/AddressSearch/AdressCellView.swift
+++ solutions/devsprint-bruno-ramos-3/DeliveryApp/Screens/AddressSearch/AdressCellView.swift
@@ -16,4 +16,44 @@ class AdressCellView: UITableViewCell {
         stack.spacing = 5
         return stack
     }()
+    
+    lazy var titleLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.text = "Rua dos Patinhos, 126"
+        return label
+    }()
+    
+    lazy var subtitleLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Limoeiro"
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubviews()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        addSubview(stackView)
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(subtitleLabel)
+    }
+
+    func configureConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+        ])
+    }
 }
