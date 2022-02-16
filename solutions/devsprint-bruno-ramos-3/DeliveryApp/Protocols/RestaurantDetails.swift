06084f7d0239db2996fecdadf3e827f3baab5002
rodrigofran <rodrigofrancms@gmail.com>
2022-02-16 17:51:06 -0300

    Ajustes na documentação do protocolo ViewCode

---
+++ solutions/devsprint-bruno-ramos-3/DeliveryApp/Protocols/RestaurantDetails.swift
@@ -0,0 +1,8 @@
+//
+//  RestaurantDetails.swift
+//  DeliveryAppChallenge
+//
+//  Created by Rodrigo Francischett Occhiuto on 15/02/22.
+//
+
+import Foundation
--- solutions/devsprint-bruno-ramos-3/DeliveryApp/Protocols/ViewCode.swift
+++ solutions/devsprint-bruno-ramos-3/DeliveryApp/Protocols/ViewCode.swift
@@ -5,12 +5,15 @@
 //  Created by Rodrigo Francischett Occhiuto on 14/02/22.
 //
 
-import UIKit
-
+/// Protocolo para implementar as funções necessárias em uma View.
 protocol ViewCode {
+    ///Chamar esta funçao para adicionar as hierarquias das views
     func setupComponents()
+    ///Chamar esta funçao  para setar as constraints das views
     func setupConstraints()
+    ///Chamar esta funçao para adicionar alguma configuração adicional caso seja necessário
     func setupExtraConfiguration()
+    ///Chamar esta função dentro da ViewDidLoad da View para implementar as configurações da extension ViewCode
     func setup()
 }
 
