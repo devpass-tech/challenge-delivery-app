5235de5386a46f61dfa310aff65af710c5bd0df7
Anderson Oliveira <anderson.oliveira@cora.com.br>
2022-04-26 16:21:35 -0300

    corrects and implements suggestions

--- solutions/devsprint-ana-nogal-1/DeliveryApp/Screens/Home/HomeViewController.swift
+++ solutions/devsprint-ana-nogal-1/DeliveryApp/Screens/Home/HomeViewController.swift
@@ -7,45 +7,37 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        print(searchController.searchBar.text)
-    }
+class HomeViewController: UIViewController {
     
-
     private let deliveryApi = DeliveryApi()
-
+    
     private let homeView: HomeView = {
-
+        
         let homeView = HomeView()
         return homeView
     }()
-
+    
     lazy var searchController: UISearchController = {
-       let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchResultsUpdater = self
+        let searchController = UISearchController()
+        searchController.searchBar.delegate = self
         searchController.searchBar.placeholder = "Nome do restaurante"
         return searchController
     }()
     
     init() {
         super.init(nibName: nil, bundle: nil)
-
-        navigationItem.title = "Delivery App"
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
-        navigationItem.searchController = searchController
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
+        setup()
         deliveryApi.fetchRestaurants { restaurants in
-
+            
             DispatchQueue.main.async {
-
+                
                 self.homeView.updateView(with: restaurants)
             }
         }
@@ -54,4 +46,16 @@ class HomeViewController: UIViewController, UISearchResultsUpdating {
     override func loadView() {
         self.view = homeView
     }
+    
+    
+    private func setup() {
+        navigationItem.title = "Delivery App"
+        navigationItem.searchController = searchController
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+
+extension HomeViewController: UISearchBarDelegate {
+    
 }
