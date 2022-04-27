97ff316a8d0ff91040e3a0e1ee7ec44dfe29f95a
Mariana Maiko <marimaiko@cora.com.br>
2022-04-27 15:15:54 -0300

    Criado celula com imagem, titulo e subtitulo

--- solutions/devsprint-ana-nogal-1/DeliveryApp/Screens/RestaurantList/RestaurantCell.swift
+++ solutions/devsprint-ana-nogal-1/DeliveryApp/Screens/RestaurantList/RestaurantCell.swift
@@ -5,4 +5,51 @@
 //  Created by Cora on 27/04/22.
 //
 
-import Foundation
+import UIKit
+
+class RestaurantCell: UIView {
+
+    lazy var image: UIImageView = {
+        let img = UIImageView()
+        img.image = UIImage(imageLiteralResourceName: "restaurant-logo")
+        img.translatesAutoresizingMaskIntoConstraints = false
+        return img
+    }()
+    
+    lazy var title: UILabel = {
+        let label = UILabel()
+        label.text = "Benjamin a Padaria"
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var subtitle: UILabel = {
+        let label = UILabel()
+        label.text = "Padaria • 23-33 min"
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.tintColor = UIColor.lightText
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    //MARK: View Configuration
+    func configViews() {
+        
+    }
+    
+    func buildViews() {
+        [image, title, subtitle].forEach(addSubview)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            image.leadingAnchor.constraint(equalTo: leadingAnchor),
+            
+            title.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: 12),
+            
+            subtitle.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: 12),
+            subtitle.topAnchor.constraint(equalTo: title.bottomAnchor)
+        ])
+    }
+}
