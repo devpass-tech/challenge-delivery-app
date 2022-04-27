257d4c26f41e893eb4115545a9e7b412a059dbf2
Anderson Oliveira <anderson.oliveira@cora.com.br>
2022-04-27 17:49:29 -0300

    basic call created. Address model created.

--- solutions/devsprint-ana-nogal-1/DeliveryApp/Screens/AddressSearch/AddressSearchViewController.swift
+++ solutions/devsprint-ana-nogal-1/DeliveryApp/Screens/AddressSearch/AddressSearchViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
-
+//NO ULTIMO COMMIT, Não upar este arquivo!!!!!!!!!!
+//NO ULTIMO COMMIT, Não upar este arquivo!!!!!!!!!! ( só pra reforçar '-' )
 class AddressSearchViewController: UIViewController {
 
+    let service = DeliveryApi()
+    
     init() {
         super.init(nibName: nil, bundle: nil)
-
+        service.getAddresses()
     }
 
     required init?(coder: NSCoder) {
--- solutions/devsprint-ana-nogal-1/DeliveryApp/Service/DeliveryApi.swift
+++ solutions/devsprint-ana-nogal-1/DeliveryApp/Service/DeliveryApi.swift
@@ -9,11 +9,35 @@ import Foundation
 
 struct DeliveryApi {
 
+    let addressURL = "https://raw.githubusercontent.com/devpass-tech/challenge-delivery-app/main/api/address_search_results.json"
+    
     func fetchRestaurants(_ completion: ([String]) -> Void) {
 
         completion(["Restaurant 1", "Restaurant 2", "Restaurant 3"])
     }
 
+    func getAddresses() {
+        print("1")
+        if let url = URL(string: addressURL) {
+            print("2")
+            print(url)
+            URLSession.shared.dataTask(with: url) { data, response, error in
+//                print("data: :: \(data)")
+//                print("response: \(response)")
+                if let data = data {
+                    do {
+                        print("3")
+                        print("Data:: : : : \(data)")
+                        let res = try JSONDecoder().decode([Address].self, from: data)
+                        print(res)
+                    } catch let error {
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
+    }
+    
     func searchAddresses(_ completion: ([String]) -> Void) {
 
         completion(["Address 1", "Address 2", "Address 3"])
