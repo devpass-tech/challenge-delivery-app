091c0dc3b04e239ba6ec2031733bdc59d9779eb4
Luiza Moruz <luizamoruz@cora.com.br>
2022-05-05 10:47:54 -0300

    Implementing MenuItemCell

--- solutions/devsprint-ana-nogal-1/DeliveryApp/Screens/MenuItem/MenuItemCell.swift
+++ solutions/devsprint-ana-nogal-1/DeliveryApp/Screens/MenuItem/MenuItemCell.swift
@@ -2,7 +2,7 @@
 //  MenuItemCell.swift
 //  DeliveryApp
 //
-//  Created by Cora on 02/05/22.
+//  Created by Luiza on 02/05/22.
 //
 
 import UIKit
@@ -16,18 +16,10 @@ final class MenuItemCell: UITableViewCell {
 
     static let identifier = "MenuItemCell"
      
-     private lazy var stackViewCell: UIStackView = {
-         let stack = UIStackView()
-         stack.axis = .vertical
-         stack.alignment = .center
-         stack.spacing = 0
-         stack.translatesAutoresizingMaskIntoConstraints = false
-         return stack
-     }()
-     
      lazy var itemTitle: UILabel = {
          let title = UILabel()
          title.font = .systemFont(ofSize: 15, weight: .regular)
+         title.numberOfLines = 0
          title.translatesAutoresizingMaskIntoConstraints = false
          return title
      }()
@@ -35,7 +27,8 @@ final class MenuItemCell: UITableViewCell {
      lazy var itemCost: UILabel = {
          let cost = UILabel()
          cost.font = .systemFont(ofSize: 13)
-         cost.textColor = UIColor.lightGray
+         cost.textColor = UIColor.gray
+         cost.numberOfLines = 0
          cost.translatesAutoresizingMaskIntoConstraints = false
          return cost
      }()
@@ -43,7 +36,8 @@ final class MenuItemCell: UITableViewCell {
     lazy var itemImage: UIImageView = {
         let image = UIImageView(image: UIImage(named: "pizza"))
         image.layer.cornerRadius = 14
-        image.frame = CGRect(x: 0, y: 0, width: 64, height: 64)
+        image.clipsToBounds = true
+        image.contentMode = .scaleAspectFill
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
@@ -73,16 +67,23 @@ final class MenuItemCell: UITableViewCell {
      }
      
      func buildViews(){
-         addSubview(stackViewCell)
-         [itemTitle,itemCost].forEach(stackViewCell.addArrangedSubview)
-         addSubview(itemImage)
+         [itemTitle,itemCost, itemImage].forEach(addSubview)
      }
      
      func setupConstraints() {
          NSLayoutConstraint.activate([
-            stackViewCell.trailingAnchor.constraint(equalTo: itemImage.leadingAnchor, constant: 32),
-            stackViewCell.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
-            itemImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15)
+        
+            itemCost.topAnchor.constraint(equalTo: centerYAnchor),
+            itemCost.leadingAnchor.constraint(equalTo: itemTitle.leadingAnchor),
+            
+            itemTitle.bottomAnchor.constraint(equalTo: centerYAnchor),
+            itemTitle.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            itemTitle.trailingAnchor.constraint(equalTo: itemImage.leadingAnchor, constant: -12),
+            
+            itemImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
+            itemImage.heightAnchor.constraint(equalToConstant: 64),
+            itemImage.widthAnchor.constraint(equalToConstant: 64),
+            itemImage.centerYAnchor.constraint(equalTo: centerYAnchor)
          ])
-}
+     }
 }
